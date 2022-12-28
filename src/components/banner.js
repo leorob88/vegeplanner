5c067e00694d7c199521d82d77b9f32f2df3d064
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-12-28 17:00:10 +0100

    update

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import Image from "../images/banner.jpg";
 
 function Banner() {
     return (
         <div>
-            <img className="banner" src={Image} alt="banner" />
+            <Link to="/"><img className="banner" src={Image} alt="banner" /></Link>
         </div>
     );
 }
