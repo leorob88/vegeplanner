25ed2f97a87a411a9a0894b14082927e7466bfdc
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-12-29 16:02:56 +0100

    update

--- src/components/singleresult.js
+++ src/components/singleresult.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function SingleResult(props) {
     const { title, time, image } = props;
     return (
-        <Link to={`/recipes/${title}`} id={title} className="recipe">
+        <Link to={`/recipes/${title}#banner`} id={title} className="recipe">
             {title}<br />
             Ready in {time} minutes<br />
             <img src={image} alt={title} className="preview" /><br /><br />
