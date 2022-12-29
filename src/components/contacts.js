b04b137bfc36556ecf49a8f4b81ad5211ad3ab46
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-12-29 13:15:23 +0100

    update

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import 'font-awesome/css/font-awesome.min.css';
+import YouTube from "../images/youtube-icon.png";
+import Twitch from "../images/twitch-icon.png";
+import FaceBook from "../images/facebook-icon.png";
+import Patreon from "../images/patreon-icon.png";
 
 function Contacts() {
     return (
         <div className='social-links'>
-            <p className='social-title'>My social links:</p>
+            <p className='social-title'>My socials:</p>
             <div className="social-content icon-zoom">
-                <a href="https://www.youtube.com/channel/UCvjnulTw54J_OtR3evmgTpQ" target="_blank" rel="noopener noreferrer" className="social"><i className="fa fa-solid fa-youtube-play red"></i></a>
+                <a href="https://www.youtube.com/channel/UCvjnulTw54J_OtR3evmgTpQ" target="_blank" rel="noopener noreferrer" className="social"><img src={YouTube} alt="youtube" /></a>
             </div>
             <div className="social-content icon-zoom">
-                <a href="https://www.twitch.tv/sunbro_gilgamesh" target="_blank" rel="noopener noreferrer" className="social"><i className="fa fa-solid fa-twitch purple"></i></a>
+                <a href="https://www.twitch.tv/sunbro_gilgamesh" target="_blank" rel="noopener noreferrer" className="social"><img src={Twitch} alt="twitch" /></a>
             </div>
             <div className="social-content icon-zoom">
-                <a href="https://www.facebook.com/sunbro.gilgamesh" target="_blank" rel="noopener noreferrer" className="social"><i className="fa fa-solid fa-facebook-official blue"></i></a>
+                <a href="https://www.facebook.com/sunbro.gilgamesh" target="_blank" rel="noopener noreferrer" className="social"><img src={FaceBook} alt="facebook" /></a>
+            </div>
+            <div className="social-content icon-zoom">
+                <a href="https://www.patreon.com/sunbrogilgamesh" target="_blank" rel="noopener noreferrer" className="social"><img src={Patreon} alt="patreon" /></a>
             </div>
         </div>
     );
