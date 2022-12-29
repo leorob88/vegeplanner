77ef024223615162111f9cd07692c9bbcbd2b10d
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-12-29 16:09:27 +0100

    update

--- src/components/singleresult.js
+++ src/components/singleresult.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from "react-router-dom";
 
 function SingleResult(props) {
     const { title, time, image } = props;
+
+    useEffect(() => {
+        setTimeout(function () {
+            const element = document.getElementById("banner");
+            element.scrollIntoView();
+        }, 1000);
+    }, []);
     return (
-        <Link to={`/recipes/${title}#banner`} id={title} className="recipe">
+        <Link to={`/recipes/${title}`} id={title} className="recipe">
             {title}<br />
             Ready in {time} minutes<br />
             <img src={image} alt={title} className="preview" /><br /><br />
